61d9d6ea29f65c1567b75744f49864dfcd90c3d8
Mohammad Arefi <m-arefi@live.com>
2022-05-04 13:35:36 +0200

    Stylings init

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,27 +1,36 @@
-import { useStates } from './utilities/states';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { useStates } from "./utilities/states";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 export default function ProductList() {
+  let s = useStates("main");
 
-  let s = useStates('main');
-
-
-  return <Container className="productList">
-    <Row><Col><h1>Products</h1></Col></Row>
-    {s.products.map(({ id, name, description, price }) =>
-      <Row className="product" key={id}>
-        <Card>
-          <Col xxl="12">
-            <h3>{name}</h3>
-          </Col>
-          <Col xxl="12">
-            <p>{description}</p>
-          </Col>
-          <Col xxl="12">
-            <p><b>Price:</b> ${price}</p>
-          </Col>
-        </Card>
-      </Row>
-    )}
-  </Container>
+  return (
+    <section className="mt-8">
+      <div class="mt-4 text-center">
+        <h3 class="text-2xl font-bold">Products</h3>
+      </div>
+      <div class="container w-full px-5 py-6 mx-auto">
+        <div class="grid lg:grid-cols-4 gap-y-6">
+          <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+            {s.products.map(({ id, name, description, price }) => (
+              <div className="product" key={id}>
+                <div xxl="12">
+                  <h3>{name}</h3>
+                </div>
+                <div xxl="12">
+                  <p>{description}</p>
+                </div>
+                <div xxl="12">
+                  <button class="px-4 py-2 bg-green-600 text-green-50">
+                    Order Now
+                  </button>
+                  <span class="text-xl text-green-600">${price}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
